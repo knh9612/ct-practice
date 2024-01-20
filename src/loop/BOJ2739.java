194f62468f483ad47edbb5169b6d9e0e5c7583e4
knh9612 <knh9612@naver.com>
2024-01-21 01:58:17 +0900

    loop start

---
+++ src/loop/BOJ2739.java
@@ -0,0 +1,19 @@
+package loop;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ2739 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+            for (int i = 1; i <= 9; i++) {
+                System.out.println(num + " * " + i + " = " + num*i);
+            }
+
+    }
+}
