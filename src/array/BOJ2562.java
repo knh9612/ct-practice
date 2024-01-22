6f98da2a2861039c0767992410bce423b4d8f34c
knh9612 <knh9612@naver.com>
2024-01-22 21:36:48 +0900

    array ing

--- src/array/BOJ2562.java
+++ src/array/BOJ2562.java
@@ -6,18 +6,22 @@ import java.io.InputStreamReader;
 
 public class BOJ2562 {
 
-//    public static void main(String[] args) throws IOException {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        String str;
-//        int[] sequence = new int[9];
-//        int count = 0;
-//
-//        while (count < 9) {
-//            str = br.readLine();
-//            for (int i = 0; i < 9; i++) {
-//                sequence[i] = Integer.parseInt(str);
-//                count++;
-//            }
-//        }
-//    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] sequence = new int[9];
+        int maxNum = 0;
+        int maxIndex = 0;
+
+        for (int i = 0; i < 9; i++) {
+            sequence[i] = Integer.parseInt(br.readLine()); // 입력 받을 때마다 배열에 값을 넣음
+        }
+        for (int i = 0; i < 9; i++) {
+            if (maxNum < sequence[i]) {
+                maxNum = sequence[i];
+                maxIndex = (i + 1);
+            }
+        }
+        System.out.println(maxNum);
+        System.out.println(maxIndex);
+    }
 }
